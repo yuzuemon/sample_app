b870fe8c97f84b1f30befaacf54f1b3276a20940
yuzuemon <killerqueen64@gmail.com>
2013-11-27 20:39:47 +0900

    5.1まで

---
+++ example_user.rb
@@ -0,0 +1,12 @@
+class User
+  attr_accessor :name, :email # attribute_accessorの定義
+
+  def initialize(attributes = {})
+    @name  = attributes[:name]
+    @email = attributes[:email]
+  end
+
+  def formatted_email
+    "#{@name} <#{@email}>"
+  end
+end
