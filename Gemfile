df60b79d1faba6020e8502b57984ac3fd2a92782
yuzuemon <killerqueen64@gmail.com>
2013-11-28 21:38:38 +0900

    Make a basic User model (including secure passwords)

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.14'
 gem 'bootstrap-sass', '2.1'
+gem 'bcrypt-ruby', '3.0.1'
 
 group :development, :test do
   gem 'sqlite3', '1.3.5'
@@ -10,6 +11,10 @@ group :development, :test do
   gem 'guard-spork', '1.2.0'
   gem 'childprocess', '0.3.6'
   gem 'spork', '0.9.2'
+end
+
+group :development do
+  gem 'annotate', '2.5.0'
 end
 
 # assetsでは使うが
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,6 +4,11 @@ describe "Static pages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1',        text: heading) }
+    it { should have_selector('title',     text: full_title(page_title)) }
+  end
+
   describe "Home page" do
     before { visit root_path }
 
@@ -31,5 +36,20 @@ describe "Static pages" do
 
     it { should have_selector('h1',        text: 'Contact') }
     it { should have_selector('title',     text: full_title('')) }
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About Us')
+    click_link "Help"
+    page.should have_selector 'title', text: full_title('Help')
+    click_link "Contact"
+    page.should have_selector 'title', text: full_title('Contact')
+    click_link "Home"
+    # click_link "Sign up now!"
+    # page.should have_selector 'title', text: full_title('Sign up')
+    # click_link "About"
+    # page.should have_selector 'title', text: full_title('About Us')
   end
 end
